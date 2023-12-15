c6c60d1e8ee0bd0b25fb232dbe2b867b4c96b98d
Harry Werkmeister <harry.werkmeister@iu.org>
2023-12-15 19:38:53 +0100

    rename functions and add function documentation

--- 2023/day4/part1.js
+++ 2023/day4/part1.js
@@ -1,42 +1,54 @@
 const fs = require("fs");
 const readline = require("readline");
 
-function intersect(a, b) {
-    var setA = new Set(a);
-    var setB = new Set(b);
-    var intersection = new Set([...setA].filter(x => setB.has(x)));
+/**
+ * Computes the intersection of two arrays.
+ * @param {Array} a - The first array.
+ * @param {Array} b - The second array.
+ * @returns {Array} The intersection of a and b.
+ */
+function computeIntersection(a, b) {
+    const setA = new Set(a);
+    const setB = new Set(b);
+    const intersection = new Set([...setA].filter(x => setB.has(x)));
     return Array.from(intersection);
-  }
-
-function splitGameLine(line) {
-    const [_, completeNumbersString] = line.split(": ");
-    const [winningNumbersString, playNumbersString] = completeNumbersString.split(" | ");
-
-    const winningNumbers = winningNumbersString.split(" ").map((n) => parseInt(n, 10)).filter((n) => !isNaN(n));
-    const playNumbers = playNumbersString.split(" ").map((n) => parseInt(n, 10)).filter((n) => !isNaN(n));
-
-    const intersection = intersect(winningNumbers, playNumbers)
-
-
-    if (intersection.length === 0) {
-        return 0
-    }
-
-    return intersection.reduce((previous, _) => previous * 2, 1) / 2
-
 }
 
-async function processFile(filePath) {
+/**
+ * Parses a line from the game file and calculates the score.
+ * @param {string} line - A line from the game file.
+ * @returns {number} The calculated score for the line.
+ */
+function calculateLineScore(line) {
+    const [_, numbersString] = line.split(": ");
+    const [winningNumbersString, playNumbersString] = numbersString.split(" | ");
+
+    const winningNumbers = winningNumbersString.split(" ").map(n => parseInt(n, 10)).filter(n => !isNaN(n));
+    const playNumbers = playNumbersString.split(" ").map(n => parseInt(n, 10)).filter(n => !isNaN(n));
+
+    const intersection = computeIntersection(winningNumbers, playNumbers);
+
+    return intersection.length === 0 ? 0 : intersection.reduce((total, _) => total * 2, 1) / 2;
+}
+
+/**
+ * Processes the game file and computes the total score.
+ * @param {string} filePath - The path to the game file.
+ */
+async function processGameFile(filePath) {
     const fileStream = fs.createReadStream(filePath);
     const rl = readline.createInterface({
         input: fileStream,
         crlfDelay: Infinity,
     });
-let result = 0
+
+    let totalScore = 0;
     for await (const line of rl) {
-        result += splitGameLine(line);
+        totalScore += calculateLineScore(line);
     }
-    console.log(result);    
+
+    console.log(`Total Score: ${totalScore}`);
 }
 
-processFile("./puzzle-input.txt");
+// Process the input file
+processGameFile("./puzzle-input.txt");
